714cbb408fb852538fe172579f6552bd49bea682
Aaron Beschorner <aaronbeschorner@gmail.com>
2023-10-31 17:36:46 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Encode a password by adding +3 to each integer.
 def encode(password):
     passwordList = list(password)
     for i in range(len(passwordList)):
@@ -10,17 +11,22 @@ def encode(password):
 
 if __name__ == '__main__':
     while True:
+        # Menu options
         print('Menu')
         print('-' * 13)
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
+        # User selects an option
         menuOption = int(input('Please enter an option: '))
+        # Encode method
         if menuOption == 1:
             passwordDecoded = input('Please enter your password to encode: ')
             passwordEncoded = encode(passwordDecoded)
             print('Your password has been encoded and stored!')
+        # Encode and decode method
         if menuOption == 2:
             print(f'The encoded password is {passwordEncoded}, and the original password is.')
+        # Exit the program
         if menuOption == 3:
             break
