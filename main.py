6292f38ec7f087bcf678edbb5d7a68c06bd724bd
Aaron Beschorner <aaronbeschorner@gmail.com>
2023-10-31 17:30:45 -0400

    Encoder

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+def encode(password):
+    passwordList = list(password)
+    for i in range(len(passwordList)):
+        passwordList[i] = int(passwordList[i]) + 3
+        if passwordList[i] > 9:
+            passwordList[i] = passwordList[i] - 10
+        passwordList[i] = str(passwordList[i])
+    passwordList = ''.join(passwordList)
+    return passwordList
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        print('Menu')
+        print('-' * 13)
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        menuOption = int(input('Please enter an option: '))
+        if menuOption == 1:
+            passwordDecoded = input('Please enter your password to encode: ')
+            passwordEncoded = encode(passwordDecoded)
+            print('Your password has been encoded and stored!')
+        if menuOption == 2:
+            print(f'The encoded password is {passwordEncoded}, and the original password is.')
+        if menuOption == 3:
+            break
